15a9dc516530183472281f5bccd6dd68553ac306
Sabbha <sabbha.power1@gmail.com>
2024-02-03 08:45:24 +0530

    Created a Hello world base command

---
+++ dataentry/management/commands/helloworld.py
@@ -0,0 +1,8 @@
+from typing import Any
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    help = 'Prints Hello World!'
+    
+    def handle(self, *args, **kwargs):
+        self.stdout.write("Hello World.")
